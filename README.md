67e331f1b5e09a13a4cf5649987d9a9bc1301421
Ubuntu <ubuntu@ip-172-31-89-162.ec2.internal>
2020-01-26 16:24:06 +0000

    updated README.md and css/style.css  index.html and maps.js

--- README.md
+++ README.md
@@ -1,4 +1,52 @@
-  # Introduction
-Hello and welcome to my miestone projec two
-Using and modifying a vareity of codes from the interactive frontend module, I've decide to create a landing page that directs the user to travel agent of their chosen destination.
-after first navigating the google map.# Milestone_Project_2
+## Looking for A Break? 
+Having pondered for hours and days on what to base this Milestone project I came up with the idea of a landing page that takes the user to their chosen destinations that are marked on the map. 
+## UX
+The project is designed around user holiday interest with the opportunity for the user to research further once they have been redirected to the area of their choice.
+It’s a very simple to use page with most of the map functionality already built into API. 
+Originally, I had intended in using buttons to direct the user to a travel booking website but as I got deeper into the projects, I experimented with trying to build more display functions into the map. 
+## Features
+Google Map API
+Has all the generic features, I have added the traffic script to display traffic when zooming in on the map.
+Mediterranien, Caribbean, Europe (labels)
+Located just below the title heading change colour on mouse hover and when selected the colour changes and remains until another selection is made.
+When selected the user is directed to another google map API specific to the area selected. The new API has a wider choice of selection for the user and also a search functionality. These extra specifications have not been constructed by me and are solely google’s coding
+## Features left to Implement
+During the construction of the project I experimented with the different codes I had learnt on the interactive front-end module and combining some with the codes from Google API Devlopers web page to try and add new features.  Due to the time constraint I had to revert back to keeping the coding simple. The code for the features was constantly changing due to the code failing and constantly having to test and rethink new ideas. 
+Weather 
+JQuery to refresh the map using the .click 
+infoWindow to display when the mark is clicked or on mouseHover (JQuery and Javascript)
+JQuery function to direct the user to the Travel booking companies (the URL is already in the HTML)
+## Technologies used
+HTML,
+CSS,
+Bootstrap,
+Fontawesome,
+Javascript
+JQuery
+
+## Testing
+A variety of testing methods were deployed, 
+i.	HTML checked https://validator.w3.org/
+ii.	Console logs during development 
+iii.	Spouse tested the functionality and observed the colour coding was too bright and conflicting with the background. Colour was changed to answer the observations.
+iv.	Mobile first testing 
+I did have some technical issues which put the project on hold however this did not deter me from trying to meet the deadline. It resulted in having to simplify the project and not to be adventurous with my thoughts and coding
+## Deployment
+There is a live version and a deployed version in Github. Several stages of the project development were deployed to Github and the one with the most current date is the final finished version.
+
+## Credits
+You Tube Map API Javascript tutorials from where I was inspired to test the google maps API examples.  The examples in Google maps developer alone are not adequate without seeing them being written and applied.  
+## Content
+In the validator it states the Labels and anchors should not be together. However, I have tried different options ul, li, buttons and cannot seem to get the right functionality. The present coding is producing the right results.  With more time I could resolve this.  I will research later to find out the correct code I somehow feel it can be resolved with a jQuery function if there was more time.
+## Media
+Google Maps Developer Tools.
+Image obtained - free pictures of sunset holiday https://www.bing.com/images/search?q=free+pictures+of+sunset+holiday&qpvt=free+pictures+of+sunset+holiday&FORM=IGRE
+
+
+## Acknowlegements
+
+Ideas were inspired by Google Maps API Javascript  videos – YouTube
+Create A Google Map In A Website | Google API Map | Learn HTML and CSS | HTML Tutorial
+
+Code Institute – Interactive Front End Module
+Code Institute - Rosie Project  
--- js/maps.js
+++ js/maps.js
@@ -1,133 +1,139 @@
 /* ----------This code is a combination of Google maps API example codes which i have modfied to the best of my abilit to suit my project needs*/
- var map;
- var service;
+var map;
+var service;
 
- function handleSearchResults(results, status) {
-  if (status == google.maps.places.PlacesServiceStatus.ok) {
-   for (var i = 0; i < results.legnth; i++) {
-    console.log(results[i]);
+function handleSearchResults(results, status) {
+ if (status == google.maps.places.PlacesServiceStatus.ok) {
+  for (var i = 0; i < results.legnth; i++) {
+   console.log(results[i]);
 
-    var place = results[i];
-    position: results[i].geometry.location;
-    map: map;
-    icon: rsults[i].icon
-   }
+   var place = results[i];
+   position: results[i].geometry.location;
+   map: map;
+   icon: rsults[i].icon
   }
  }
+}
 
- function performSearch() {
-  var request = {
-   bound: map.getBounds(),
-   name: "Recreation"
-  };
-  services.nearbySearch(request, handleSearchResults);
- }
+function performSearch() {
+ var request = {
+  bound: map.getBounds(),
+  name: "Recreation"
+ };
+ 
+ services.nearbySearch(request, handleSearchResults);
+ 
+}
 
- function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-   zoom: 4,
-   center: {
-    lat: 35.746512,
-    lng: -39.462891 
-   }
-   
-  });
-   var trafficLayer = new google.maps.TrafficLayer();
-             trafficLayer.setMap(map);
-    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var locations = [
-   { lat: 37.688167, lng: 23.466797 },
-   { lat: 19.440694, lng: -73.212891 },
-   { lat: 47.846344, lng: 7.119141 },
-   { lat: 22.665977, lng: -82.245074 },
-   { lat: 52.520008, lng: 13.404954 },
-   { lat: 37.983810, lng: 23.727539 },
-   { lat: 48.856613, lng: 2.352222 },
-   { lat: 13.096851, lng: -59.614483 },
-   { lat: 35.185566, lng: 33.382275 },
-   { lat: 18.735693, lng: -70.162651 }
-  ];
+function initMap() {
+ map = new google.maps.Map(document.getElementById("map"), {
+  zoom: 4,
+  center: {
+   lat: 35.746512,
+   lng: -39.462891
+  }
 
-  var markers = locations.map(function(location, i) {
-   return new google.maps.Marker({
-    position: location,
-    label: labels[i % labels.length]
-   });
-  });
-  var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js' });
+ });
  
  
+ 
+ var trafficLayer = new google.maps.TrafficLayer();
+
+ trafficLayer.setMap(map);
+
+ var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+ var locations = [
+  { lat: 37.688167, lng: 23.466797 },
+  { lat: 19.440694, lng: -73.212891 },
+  { lat: 47.846344, lng: 7.119141 },
+  { lat: 22.665977, lng: -82.245074 },
+  { lat: 52.520008, lng: 13.404954 },
+  { lat: 37.983810, lng: 23.727539 },
+  { lat: 48.856613, lng: 2.352222 },
+  { lat: 13.096851, lng: -59.614483 },
+  { lat: 35.185566, lng: 33.382275 },
+  { lat: 18.735693, lng: -70.162651 }
+ ];
+
+ var markers = locations.map(function(location, i) {
+  return new google.maps.Marker({
+   position: location,
+   label: labels[i % labels.length]
+  });
+  
+ });
+ 
+ var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js' });
+
+
  services = new google.maps.places.PlacesService(map);
 
  google.maps.event.addListenerOnce(map, "bounds_changed", performSearch);
 
  $("#refresh").click(getBounds);
+ 
 }
 
- $(document).ready(function() 
- {
-  navigator.geolocation.getLocations( );
+$(document).ready(function() {
+ navigator.geolocation.getLocations();
 
+});
+
+var input = document.getElementById('pac-input');
+var searchBox = new google.maps.places.SearchBox(input);
+map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+map.addListener('bounds_changed', function() {
+ searchBox.setBounds(map.getBounds());
+});
+
+var markers = [];
+
+searchBox.addListener('places_changed', function() {
+ var places = searchBox.getPlaces();
+
+ if (places.length == 0) {
+  return;
+ }
+
+ markers.forEach(function(marker) {
+  marker.setMap(null);
  });
-
-      var input = document.getElementById('pac-input');
-        var searchBox = new google.maps.places.SearchBox(input);
-        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
-
-           map.addListener('bounds_changed', function() {
-          searchBox.setBounds(map.getBounds());
-        });
-
-        var markers = [];
-        
-        searchBox.addListener('places_changed', function() {
-          var places = searchBox.getPlaces();
-
-          if (places.length == 0) {
-            return;
-          }
-
-          markers.forEach(function(marker) {
-          marker.setMap(null);
-          });
-          markers = [];
-
-         
-          var bounds = new google.maps.LatLngBounds();
-          places.forEach(function(place) {
-            if (!place.geometry) {
-              console.log("Returned place contains no geometry");
-              return;
-            }
-            var icon = {
-              url: place.icon,
-              size: new google.maps.Size(71, 71),
-              origin: new google.maps.Point(0, 0),
-              anchor: new google.maps.Point(17, 34),
-              scaledSize: new google.maps.Size(25, 25)
-            };
-
-            
-            markers.push(new google.maps.Marker({
-              map: map,
-              icon: icon,
-              title: place.name,
-              position: place.geometry.location
-            }));
-
-            if (place.geometry.viewport) {
-             
-              bounds.union(place.geometry.viewport);
-            } else {
-              bounds.extend(place.geometry.location);
-            }
-          });
-          map.fitBounds(bounds);
-         
-      });
-
-   
+ markers = [];
 
 
+ var bounds = new google.maps.LatLngBounds();
+ places.forEach(function(place) {
+  if (!place.geometry) {
+   console.log("Returned place contains no geometry");
+   return;
+  }
+  var icon = {
+   url: place.icon,
+   size: new google.maps.Size(71, 71),
+   origin: new google.maps.Point(0, 0),
+   anchor: new google.maps.Point(17, 34),
+   scaledSize: new google.maps.Size(25, 25)
+  };
 
- 
+  markers.push(new google.maps.Marker({
+   map: map,
+   icon: icon,
+   title: place.name,
+   position: place.geometry.location
+  }));
+
+  if (place.geometry.viewport) {
+
+   bounds.union(place.geometry.viewport);
+  }
+  
+  else {
+   bounds.extend(place.geometry.location);
+  }
+ });
+ map.fitBounds(bounds);
+
+});
+
