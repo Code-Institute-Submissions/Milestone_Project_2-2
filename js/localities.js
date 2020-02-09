a46b90c0b3171210989b01a7ca2e1dba43e3bc57
isiko1 <lutersweg@hotmail.com>
2020-02-09 22:57:28 +0000

    added search functionanlity

---
+++ js/localities.js
@@ -0,0 +1,66 @@
+/*var map;
+function initAutocomplete(){
+let location = new Object();
+navigator.geolocation.getCurrentPosition(function(pos){
+location.lat = pos.coords.latitude;
+location.long = pos.coords.longitude;
+map = new google.maps.Map(document.getElementById("map"),{
+    center: {lat:  35.746512,lng:-39.462891},
+    zoom: 15
+});
+getRestaurants(location);
+    });
+};
+
+function getRestaurants (location) {
+    var searchBox = new google.maps.LatLng(location.lat,location.long);
+    var request = {
+        location: searchBox,
+        radius: '5000',
+        type: ['restaurant']
+    };
+   var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch(request,callback);
+};
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for(var i = 0; i < results.length; i++) {
+        console.log (results)
+            var place = results[i];
+            let price = createPrice(place.price_level);
+        let content = `<h3>${place.name}</h3>
+        <h4>${place.vicinity}</h4>
+        <p>Price:${price}<br/>
+        Rating: ${place.rating}`;
+
+        var marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name,
+            });
+        var infowindow = new google.maps.InfoWindow({
+            content: content
+        });
+        bindInfoWindow(marker, map, infowindow, content);
+        marker.setMap(map)
+
+        };
+    };
+};
+function bindInfoWindow(marker, map, infowindow, html) {
+   marker.addListener('click', function() {
+       infowindow.setContent(html);
+       infowindow.open(map, this);
+   }); 
+};
+function createPrice(level) {
+    if(level != "" && level !=null) {
+        let out = "";
+        for (var x = 0; x < level; x++) {
+            out += "$";
+        };
+        return out;
+    } else {
+        return "?";
+    };
+};
--- js/maps.js
+++ js/maps.js
@@ -1,46 +1,21 @@
 /* ----------This code is a combination of Google maps API example codes which i have modfied to the best of my abilit to suit my project needs*/
-var map;
+/*var map;
 var service;
-
-function handleSearchResults(results, status) {
- if (status == google.maps.places.PlacesServiceStatus.ok) {
-  for (var i = 0; i < results.legnth; i++) {
-   console.log(results[i]);
-
-   var place = results[i];
-   position: results[i].geometry.location;
-   map: map;
-   icon: rsults[i].icon
-  }
- }
-}
-
-function performSearch() {
- var request = {
-  bound: map.getBounds(),
-  name: "Recreation"
- };
- 
- services.nearbySearch(request, handleSearchResults);
- 
-}
-
+var infowindow;
 function initMap() {
+    var options = new google.maps.LatLng(37.688167,  23.466797)
  map = new google.maps.Map(document.getElementById("map"), {
   zoom: 4,
   center: {
    lat: 35.746512,
    lng: -39.462891
-  }
-
+  },
+  mapTypeId: 'roadmap'
+  
  });
- 
- 
- 
- var trafficLayer = new google.maps.TrafficLayer();
-
+  var trafficLayer = new google.maps.TrafficLayer();
  trafficLayer.setMap(map);
-
+  
  var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
  
  var locations = [
@@ -55,85 +30,62 @@ function initMap() {
   { lat: 35.185566, lng: 33.382275 },
   { lat: 18.735693, lng: -70.162651 }
  ];
-
  var markers = locations.map(function(location, i) {
   return new google.maps.Marker({
    position: location,
    label: labels[i % labels.length]
   });
-  
- });
- 
- var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js' });
-
-
- services = new google.maps.places.PlacesService(map);
-
- google.maps.event.addListenerOnce(map, "bounds_changed", performSearch);
-
- $("#refresh").click(getBounds);
- 
+   
+  });
+  var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js' });
 }
+ var request = options
+    radius: '1500',
+    type ['restaurant']
 
-$(document).ready(function() {
- navigator.geolocation.getLocations();
+  service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 
-});
+function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+        for(var i = 0; i < results.length; i++) {
+        console.log (results)
+            var place = results[i];
+            let price = createPrice(place.price_level);
+        let content = `<h3>${place.name}</h3>
+        <h4>${place.vicinity}</h4>
+        <p>Price:${price}<br/>
+        Rating: ${place.rating}`;
 
-var input = document.getElementById('pac-input');
-var searchBox = new google.maps.places.SearchBox(input);
-map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+        var marker = new google.maps.Marker({
+            position: place.geometry.location,
+            map: map,
+            title: place.name,
+            });
+        var infowindow = new google.maps.InfoWindow({
+            content: content
+        });
+        bindInfoWindow(marker, map, infowindow, content);
+        marker.setMap(map)
 
-map.addListener('bounds_changed', function() {
- searchBox.setBounds(map.getBounds());
-});
-
-var markers = [];
-
-searchBox.addListener('places_changed', function() {
- var places = searchBox.getPlaces();
-
- if (places.length == 0) {
-  return;
- }
-
- markers.forEach(function(marker) {
-  marker.setMap(null);
- });
- markers = [];
-
-
- var bounds = new google.maps.LatLngBounds();
- places.forEach(function(place) {
-  if (!place.geometry) {
-   console.log("Returned place contains no geometry");
-   return;
-  }
-  var icon = {
-   url: place.icon,
-   size: new google.maps.Size(71, 71),
-   origin: new google.maps.Point(0, 0),
-   anchor: new google.maps.Point(17, 34),
-   scaledSize: new google.maps.Size(25, 25)
-  };
-
-  markers.push(new google.maps.Marker({
-   map: map,
-   icon: icon,
-   title: place.name,
-   position: place.geometry.location
-  }));
-
-  if (place.geometry.viewport) {
-
-   bounds.union(place.geometry.viewport);
-  }
-  
-  else {
-   bounds.extend(place.geometry.location);
-  }
- });
- map.fitBounds(bounds);
-
-});
+        };
+    };
+};
+function bindInfoWindow(marker, map, infowindow, html) {
+   marker.addListener('click', function() {
+       infowindow.setContent(html);
+       infowindow.open(map, this);
+   }); 
+};
+function createPrice(level) {
+    if(level != "" && level !=null) {
+        let out = "";
+        for (var x = 0; x < level; x++) {
+            out += "$";
+        };
+        return out;
+    } else {
+        return "?";
+    };
+};
 
